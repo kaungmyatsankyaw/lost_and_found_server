9a5198b1d6fb2723b22c86d3856725aa0114985d
kaungmyatsankyaw <kaungmyatsan154996@gmail.comm>
2020-11-02 15:37:01 +0630

    Item Update

--- app/Http/Controllers/Api/Item/IndexController.php
+++ app/Http/Controllers/Api/Item/IndexController.php
@@ -112,7 +112,7 @@ class IndexController extends Controller
 
         $_item = Item::where('id', $_item_id)->first()->makeHidden(['user']);
        
-        return $_item;
+        // return $_item;
         // $_item->user_id = $_user_id;
         $_item->name = $_item_name;
         $_item->item = $_item;
@@ -121,10 +121,10 @@ class IndexController extends Controller
         $_item->address = $_item_address;
         $_item->time = $_item_found_time;
 
-        // if (!empty($_lat) && !empty($_lng)) {
-        //     $_item->location = \DB::raw("ST_GeomFromText('POINT(${_lat} ${_lng})')");
+        if (!empty($_lat) && !empty($_lng)) {
+            $_item->location = \DB::raw("ST_GeomFromText('POINT(${_lat} ${_lng})')");
 
-        // }
+        }
 
         $_item->update();
 
